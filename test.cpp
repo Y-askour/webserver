71f3f918c9ffe5d57f776ad3ab99ad1585d830ca
Younes Askour <pausedarkf@gmail.com>
2023-05-22 16:27:49 +0100

    learning

--- test.cpp
+++ test.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -5,11 +6,22 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <iostream>
+#include <unistd.h>
 
 int main (int argc, char *argv[])
 {
 	char dst[INET_ADDRSTRLEN];
 	struct addrinfo *servinfo;
+	struct sockaddr_in use_bind;
+
+	use_bind.sin_family = AF_INET;
+	use_bind.sin_port = htons(7777);
+
+	//use_bind.sin_addr.s_addr = inet_addr("10.12.5.4");
+	// this code commented is the same as the code above
+	use_bind.sin_addr.s_addr =  INADDR_ANY;
+
+	memset(use_bind.sin_zero,0,sizeof(use_bind.sin_zero));
 
 	if (argc != 2)
 		return 1;
@@ -24,9 +36,16 @@ int main (int argc, char *argv[])
 			struct sockaddr_in *ip = (struct sockaddr_in*) servinfo->ai_addr;
 			void *addr = &(ip->sin_addr);
 			inet_ntop(servinfo->ai_family,addr,dst,sizeof dst);
+
+
+			int s = socket(PF_INET,SOCK_STREAM, 0);
+			
+			if  (bind(s,(struct sockaddr*) &use_bind ,sizeof(use_bind)) == -1)
+				return (89);
+
+			close(s);
 			std::cout << dst << std::endl;
 		}	
 	}
-	freeaddrinfo(servinfo);
 	return 0;
 }
