9532be5b9098cee037e89aa120c5ce6ac9386a05
Younes Askour <pausedarkf@gmail.com>
2023-05-22 18:51:40 +0100

    learning

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <netinet/in.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -10,42 +11,31 @@
 
 int main (int argc, char *argv[])
 {
-	char dst[INET_ADDRSTRLEN];
-	struct addrinfo *servinfo;
-	struct sockaddr_in use_bind;
-
-	use_bind.sin_family = AF_INET;
-	use_bind.sin_port = htons(7777);
-
-	//use_bind.sin_addr.s_addr = inet_addr("10.12.5.4");
-	// this code commented is the same as the code above
-	use_bind.sin_addr.s_addr =  INADDR_ANY;
-
-	memset(use_bind.sin_zero,0,sizeof(use_bind.sin_zero));
+	struct addrinfo hints, *res;
+	char ipstr[INET_ADDRSTRLEN];
 
 	if (argc != 2)
-		return 1;
+		return -1;
+	memset(&hints, 0,sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
-	if (getaddrinfo(argv[1], "http", NULL,&servinfo))
-		return 1;
+	if (getaddrinfo(argv[1], "http", &hints, &res))
+		return 2;
 
-	for (;servinfo != NULL; servinfo = servinfo->ai_next)
+	void *addr;
+	if (res->ai_family == AF_INET)
 	{
-		if (servinfo->ai_family == AF_INET)
-		{
-			struct sockaddr_in *ip = (struct sockaddr_in*) servinfo->ai_addr;
-			void *addr = &(ip->sin_addr);
-			inet_ntop(servinfo->ai_family,addr,dst,sizeof dst);
-
-
-			int s = socket(PF_INET,SOCK_STREAM, 0);
-			
-			if  (bind(s,(struct sockaddr*) &use_bind ,sizeof(use_bind)) == -1)
-				return (89);
-
-			close(s);
-			std::cout << dst << std::endl;
-		}	
+		struct sockaddr_in *ipv4 = (struct sockaddr_in *) res->ai_addr;
+		addr = &(ipv4->sin_addr);
 	}
+	inet_ntop(res->ai_family, addr, ipstr, sizeof ipstr);
+	std::cout << "connecting to " << argv[1] << " with ip : " << ipstr << std::endl;
+
+	int sockefd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+	if (connect(sockefd, res->ai_addr, res->ai_addrlen))
+		return 3;
+	while(1);
+
 	return 0;
 }
