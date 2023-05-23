c2955b4131891b1f46de21a901ded38fdcdd6a8f
Younes Askour <pausedarkf@gmail.com>
2023-05-23 17:17:01 +0100

    creating a simple server

--- test.cpp
+++ test.cpp
@@ -11,30 +11,37 @@
 
 int main (int argc, char *argv[])
 {
+	(void)argc;
+	(void)argv;
+
+	std::cout << "the server has started" << std::endl;
 	struct addrinfo hints, *res;
-	char ipstr[INET_ADDRSTRLEN];
+	struct sockaddr communication;
 
-	if (argc != 2)
-		return -1;
 	memset(&hints, 0,sizeof hints);
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
-	if (getaddrinfo(argv[1], "http", &hints, &res))
+	if (getaddrinfo(NULL, "8080", &hints, &res))
+		return 1;
+
+	int sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+	if (bind(sockfd,res->ai_addr,res->ai_addr->sa_len))
 		return 2;
+	std::cout << "the server waits for a request" << std::endl;
 
-	void *addr;
-	if (res->ai_family == AF_INET)
-	{
-		struct sockaddr_in *ipv4 = (struct sockaddr_in *) res->ai_addr;
-		addr = &(ipv4->sin_addr);
-	}
-	inet_ntop(res->ai_family, addr, ipstr, sizeof ipstr);
-	std::cout << "connecting to " << argv[1] << " with ip : " << ipstr << std::endl;
+	listen(sockfd,20);
+	socklen_t addr_size = sizeof communication;
+	int fd_com = accept(sockfd, &communication, &addr_size);
+	void *buf = NULL;
+	recv(fd_com,buf,100000,0);
+	std::cout << "the server recv : " << buf << std::endl;
+	send(fd_com,"hey user",8,0);
+	shutdown(fd_com,2);
+	close(fd_com);
+	std::cout << " the server stop" << std::endl;
 
-	int sockefd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-	if (connect(sockefd, res->ai_addr, res->ai_addrlen))
-		return 3;
 	while(1);
 
 	return 0;
