be2505e5d6ec48068576b37133f2d129d7900533
Younes Askour <pausedarkf@gmail.com>
2023-05-23 17:50:31 +0100

    creating a simple http server

--- test.cpp
+++ test.cpp
@@ -14,7 +14,7 @@ int main (int argc, char *argv[])
 	(void)argc;
 	(void)argv;
 
-	std::cout << "the server has started" << std::endl;
+	std::cout << "--------------- the server has started --------------" << std::endl << std::endl;
 	struct addrinfo hints, *res;
 	struct sockaddr communication;
 
@@ -30,19 +30,23 @@ int main (int argc, char *argv[])
 	if (bind(sockfd,res->ai_addr,res->ai_addr->sa_len))
 		return 2;
 	std::cout << "the server waits for a request" << std::endl;
+	std::cout << std::endl << std::endl;
 
 	listen(sockfd,20);
 	socklen_t addr_size = sizeof communication;
 	int fd_com = accept(sockfd, &communication, &addr_size);
-	void *buf = NULL;
+	char buf[10000];
 	recv(fd_com,buf,100000,0);
-	std::cout << "the server recv : " << buf << std::endl;
-	send(fd_com,"hey user",8,0);
+	std::cout << "the server recv : " << std::endl << std::endl << buf << std::endl;
+	std::string msg = "HTTP/1.1 200 OK\r\n";
+	msg += "Content-Type: text/html\r\n";
+	msg += "\r\n";
+	msg += "<html>  <body> <h1> younes nadi<h1> <h2> hicham nadi bl7e9 younes hssen</h2> </body> </html>\r\n";
+	send(fd_com,msg.c_str(),strlen(msg.c_str()),0);
 	shutdown(fd_com,2);
+	shutdown(sockfd,2);
 	close(fd_com);
-	std::cout << " the server stop" << std::endl;
-
-	while(1);
-
+	close(sockfd);
+	std::cout << "the server stop" << std::endl;
 	return 0;
 }
