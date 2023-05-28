d5a6eb0ad634dfa7e2985bd1337c6578e7df65ca
Younes Askour <pausedarkf@gmail.com>
2023-05-28 20:35:24 +0100

    starting the parssing branch

---
+++ include/webserv.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <iostream>
+void parse_config_file();
--- parssing/parse.cpp
+++ parssing/parse.cpp
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   parse.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yaskour <yaskour@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/28 20:23:13 by yaskour           #+#    #+#             */
-/*   Updated: 2023/05/28 20:35:00 by yaskour          ###   ########.fr       */
+/*   Created: 2023/05/28 20:33:10 by yaskour           #+#    #+#             */
+/*   Updated: 2023/05/28 20:33:49 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/webserv.hpp"
+void parse_config_file()
+{
 
-int main (int argc, char *argv[])
-{	
-	if (!(argc > 2))
-	{
-		std::cout << argv[argc - 1] << std::endl;
-		parse_config_file();
-		return 0;
-	}
-	return -1;
 }
